533c488c009ab6844f07b3f43ec0ff06374ed7ea
Tomonori HIRATA <hirata@sqlab.jp>
2020-12-28 16:55:40 +0900

    update: unbounded_fun_in_and

--- nishide_protocol.py
+++ nishide_protocol.py
@@ -322,6 +322,180 @@ def sub_unbounded_fan_in_or(bit_share_list1, bit_share_list2, bit_share_list3): 
     return ans
 
 
+def sub_unbounded_fan_in_and(bit_share_list1, bit_share_list2, bit_share_list3): # test: ok
+    # 1: ok
+    A1_share, A2_share, A3_share = 1, 0, 0
+    for i in bit_share_list1:
+        A1_share = add(A1_share, i)
+    for i in bit_share_list2:
+        A2_share = add(A2_share, i)
+    for i in bit_share_list3:
+        A3_share = add(A3_share, i)
+
+    # 3
+    b1_share_list = []
+    b1_dash_share_list = []
+    b2_share_list = []
+    b2_dash_share_list = []
+    b3_share_list = []
+    b3_dash_share_list = []
+    for i in range(0, BIT):
+        share_r1, share_r2, share_r3 = sub_rns()
+        share_dash_r1, share_dash_r2, share_dash_r3 = sub_rns()
+        b1_share_list.append(share_r1)
+        b2_share_list.append(share_r2)
+        b3_share_list.append(share_r3)
+        b1_dash_share_list.append(share_dash_r1)
+        b2_dash_share_list.append(share_dash_r2)
+        b3_dash_share_list.append(share_dash_r3)
+
+    # 4
+    B1_share_list = []
+    B2_share_list = []
+    B3_share_list = []
+    for i in range(0, BIT):
+        share_1, share_2, share_3 = share_mult(
+            b1_share_list[i], b1_dash_share_list[i], b2_share_list[i],
+            b2_dash_share_list[i], b3_share_list[i], b3_dash_share_list[i])
+        B1_share_list.append(share_1)
+        B2_share_list.append(share_2)
+        B3_share_list.append(share_3)
+
+    B_list = []
+    for i in range(0, BIT):
+        ret = add(B1_share_list[i], add(B2_share_list[i], B3_share_list[i]))
+        B_list.append(ret)
+
+    # 5
+    b1_inverse_share_list = []
+    b2_inverse_share_list = []
+    b3_inverse_share_list = []
+    for i in range(0, BIT):
+        b1_inverse_share_list.append(
+            mult(pow(B_list[i], -1, MOD_P), b1_dash_share_list[i]))
+        b2_inverse_share_list.append(
+            mult(pow(B_list[i], -1, MOD_P), b2_dash_share_list[i]))
+        b3_inverse_share_list.append(
+            mult(pow(B_list[i], -1, MOD_P), b3_dash_share_list[i]))
+
+    # 6
+    # c1_share_list = []
+    # c2_share_list = []
+    # c3_share_list = []
+    c_list = []
+    for i in range(0, BIT):
+        if i == 0:
+            share_1, share_2, share_3 = share_mult(
+                A1_share, b1_inverse_share_list[i],
+                A2_share, b2_inverse_share_list[i],
+                A3_share, b3_inverse_share_list[i]
+            )
+            c_list.append(restore(share_1, share_2, share_3))
+        else:
+            share_1, share_2, share_3 = share_mult(
+                A1_share, b1_inverse_share_list[i],
+                A2_share, b2_inverse_share_list[i],
+                A3_share, b3_inverse_share_list[i]
+            )
+            share_1, share_2, share_3 = share_mult(
+                share_1, b1_share_list[i-1],
+                share_2, b2_share_list[i-1],
+                share_3, b3_share_list[i-1]
+            )
+            c_list.append(restore(share_1, share_2, share_3))
+
+    A1_exp_list = []
+    A2_exp_list = []
+    A3_exp_list = []
+    t = 1
+    for i in range(0, BIT):
+        t = mult(t, c_list[i])
+        A1_exp_list.append(mult(t, b1_share_list[i]))
+        A2_exp_list.append(mult(t, b2_share_list[i]))
+        A3_exp_list.append(mult(t, b3_share_list[i]))
+
+    # ラグランジュの未定係数法
+    and_list = [
+        1,
+        1095567251290110014,
+        15603599130927532848,
+        3927973679664754768,
+        13253920145236317331,
+        5072689204291235733,
+        3435840349470671764,
+        16549778763406408630,
+        12459576446030141042,
+        10696557716079594807,
+        3197018912709867365,
+        9970630734852788482,
+        10106868463622397398,
+        6870526778099185113,
+        16162773849234898198,
+        6512084038109877353,
+        17791231165580233294,
+        4939553442392971102,
+        6613779837673282520,
+        6112835044719072700,
+        11561852329015516476,
+        3241087653075867336,
+        11115359391888299599,
+        1734929295488186025,
+        11958404333307668373,
+        1438215050425705879,
+        11073690707525551423,
+        17532628264929026480,
+        6082710703763137390,
+        5125529121156609847,
+        3536240549677738615,
+        168123567554747533,
+        17222647251913610444,
+        6314115150414057577,
+        5720266728093558989,
+        11315745394801430483,
+        13585209705064903598,
+        10609659846084906928,
+        6341781282790677798,
+        394864220565298862,
+        5249871464994426133,
+        13296832158245447365,
+        12801767259528789327,
+        8166545265620715462,
+        12622067858020089155,
+        10852787575795086238,
+        15358952924301378654,
+        14141418894305759068,
+        13215619082908852856,
+        9900973216959770499,
+        11398882017370494561,
+        1875791515713213273,
+        18179510337970552075,
+        12349041994205078879,
+        4559441349647564738,
+        4543633939816633313,
+        9247229033112764012,
+        9230709934058803069,
+        12860062884571458050,
+        14898881793951465380,
+        9282848170176021024,
+        16257860633325038337,
+        546853837372292893,
+        3893473855680099452,
+        672143786416464336
+    ]
+
+    share_f1, share_f2, share_f3 = 0, 0, 0
+    for index, value in enumerate(and_list):
+        if index == 0:
+            share_f1 = add(share_f1, value)
+        else:
+            share_f1 = add(share_f1, mult(value, A1_exp_list[index-1]))
+            share_f2 = add(share_f2, mult(value, A2_exp_list[index-1]))
+            share_f3 = add(share_f3, mult(value, A3_exp_list[index-1]))
+
+    ans = add(share_f1, add(share_f2, share_f3))
+    return ans
+
+
 def f_or(x):
     ans = 0
     for j in range(2, BIT+2):
@@ -364,6 +538,6 @@ if __name__ == '__main__':
             print(ret, end="\n")
         else:
             print(ret, end=", ")
-    ans = sub_unbounded_fan_in_or(shares1, shares2, shares3)
+    ans = sub_unbounded_fan_in_and(shares1, shares2, shares3)
     print(ans)
     print(MOD_P)
